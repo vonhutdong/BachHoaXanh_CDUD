c7cb3693394281a4e5d908ae9143f37e08bea30c
vonhutdong <162559123+vonhutdong@users.noreply.github.com>
2025-09-17 21:59:56 +0700

    create projet

---
+++ Project_CDUD_BachHoaXanh/Project_CDUD_BachHoaXanh/Frm_DangNhap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_CDUD_BachHoaXanh
+{
+    public partial class Frm_DangNhap : Form
+    {
+        public Frm_DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            Frm_TrangChu frm_TrangChu = new Frm_TrangChu();
+            frm_TrangChu.Show();
+        }
+    }
+}
